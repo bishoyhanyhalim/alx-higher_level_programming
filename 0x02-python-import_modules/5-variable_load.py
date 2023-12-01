62a97c8ae5d6194454d2720b8559957342cb9801
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-11-30 16:09:38 -0800

    test 2

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	from variable_load_5 import numbers
+    """it gave the value of numbers
+    """
+    from variable_load_5 import numbers
 
-	print(numbers)
+    print(numbers)

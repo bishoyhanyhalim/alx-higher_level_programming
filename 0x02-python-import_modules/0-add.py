f0b4bc928d5044ed4cbd3406076905dfe6796317
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-11-30 16:24:48 -0800

    task 00

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""this func printing total of number"""
+    """this func printing total of number"""
 
-	from add_0 import add
+    from add_0 import add
 
-	a = 1
-	b = 2
+    a = 1
+    b = 2
 
-	print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+

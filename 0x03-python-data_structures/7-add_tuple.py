ad42cb224077d23c775545950ffb387b6be70761
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-12-04 17:53:46 -0800

    task 7-

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    moons = ()
+    kools = tuple_a + (0, 0)
+    looks = tuple_b + (0, 0)
+    moons = kools[0] + looks[0], kools[1] + looks[1]
+    return (moons)

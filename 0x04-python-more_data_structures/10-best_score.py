a3f0e93d6d94acbd12ff17be6dc9b43b95405a8d
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-12-08 17:56:54 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
-def best_lokes(a_dictionary):
-
+def best_score(a_dictionary):
     """
     for return the end value
     """
+
     if a_dictionary:
 
-        hellos = list(a_dictionary.keys())
-        lokes = 0
+        my_list = list(a_dictionary.keys())
 
-        books = ""
+        leader = ""
+        score = 0
 
-        for i in hellos:
+        for i in my_list:
 
-            if a_dictionary[i] > lokes:
+            if a_dictionary[i] > score:
 
-                lokes = a_dictionary[i]
-                books = i
+                score = a_dictionary[i]
+                leader = i
 
-        return (books)
+        return (leader)

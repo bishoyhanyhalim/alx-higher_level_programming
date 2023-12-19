54ec47eb2125ee2013b9490c73e10257a8259b0b
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-12-18 17:32:43 -0800

    task 3 done

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-
 def safe_print_division(a, b):
 
     try:
         book = (a / b)
 
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         book = None
 
     finally:

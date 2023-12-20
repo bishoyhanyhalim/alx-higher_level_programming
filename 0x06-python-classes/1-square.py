1aa90efaccb102e23bb6a2de82d4cecd645f0458
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-12-19 16:13:30 -0800

    task 2 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""this is a new class good"""
+
+
+class Square:
+    """write new self size input"""
+
+    def __init__(self, size):
+        """so good class
+
+        Args:
+            this is the space of arg
+        """
+        self.__size = size

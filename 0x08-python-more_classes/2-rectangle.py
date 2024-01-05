16bb007e8319a8f3e2b9786ba415cc11ad0ff208
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-04 18:19:37 -0800

    task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""this is an class super"""
+
+
+class Rectangle:
+
+    """this is an class super"""
+
+    def __init__(self, width=0, height=0):
+        """this is an class super good
+
+        Args:
+            this is an arg func
+        """
+
+        self.width = width
+
+        self.height = height
+
+    @property
+    def width(self):
+        """this is an step class setter"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """this is an step class setter"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Return finally value"""
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return finally value"""
+
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+
+        return ((self.__width * 2) + (self.__height * 2))

ba4b590d3756a7069e39885c745ee46652fc17dd
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:32:45 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""this  class so cool"""
+
+
+class BaseGeometry:
+    """this  class so cool"""
+
+    def area(self):
+        """return the finally result ok"""
+
+        raise Exception("area() is not implemented")

46deb82369247c0a64733a7b104183169b2a9538
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:17:22 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""this is a new class and func"""
+
+
+class MyList(list):
+    """this is a new class and func"""
+
+    def __init__(self):
+        """return the finally result ok"""
+
+        super().__init__()
+
+    def print_sorted(self):
+        """return the finally result ok"""
+
+        print(sorted(self))

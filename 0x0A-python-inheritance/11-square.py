7a1696428e2a0b2d58d7603f62624990a73d9ff9
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:51:45 -0800

    taks 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""this is a new class and func"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """this is a new class and func"""
+
+    def __init__(self, size):
+        """return the finally result ok
+
+        Args:
+            this is an arf
+        """
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+        self.__size = size

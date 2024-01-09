54728072ccab65f6036da55d42fcdddc6463bbe4
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:43:31 -0800

    task 7 tany

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
+
 """this is a new class and func"""
 
 
 class BaseGeometry:
-    """this  class so cool"""
+    """this is a new class and func"""
 
     def area(self):
-        """this  class so cool"""
-
+        """this is a new class and func"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """return the finally result ok
+        """this is a new class and func
 
         Args:
-            this is an arf
-        Returns:
-            return the finally result ok
-
+            name (str): this si an str name
+            value (int): this is an vlaue
+        Raises:
+            TypeError: this is an error
+            ValueError: this is an error
         """
 
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

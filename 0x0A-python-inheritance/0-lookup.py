e67d23df3f3f61c54db45f4ffc56ce21f2a53728
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:15:00 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""this is a new class and func"""
+
+
+def lookup(obj):
+    """return the finally result ok"""
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is task or python

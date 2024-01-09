209037cf987449ef4cd4dc3339a879263a9061bd
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-08 18:25:27 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""this is a new class and func"""
+
+
+def is_same_class(obj, a_class):
+    """return the finally result ok
+
+    Args:
+        this is an arf
+    Returns:
+        return the finally result ok
+
+    """
+
+    if type(obj) == a_class:
+        return (True)
+
+    return (False)

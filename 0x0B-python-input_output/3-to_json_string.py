41ecfc01763b6154eaa5698d1e4e8c61ad6b3c48
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-18 01:43:48 -0800

    task 3 done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this is to convert string to json"""
+import json
+
+
+def to_json_string(my_obj):
+    """ this is print json file
+
+    Args:
+        this is print json file
+
+    Returns:
+        return the json string finally
+    """
+
+    return (json.dumps(my_obj))

c5657e29896201a44bc06e90265afd22eccbbe89
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-18 00:52:38 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this func to print the text count"""
+
+
+def write_file(filename="", text=""):
+    """this func to print the text count
+
+    Args:
+        this func to print the text count args
+    """
+
+    with open(filename, "w", encoding="utf-8") as text_file:
+
+        print(text_file.write(text))

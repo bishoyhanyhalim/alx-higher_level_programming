3d0bca931fd4f5ef39265176ab63fa4db7ffbd48
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-01-23 11:08:36 -0800

    task 100-

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this is an func"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """this is a text file
+
+    Args:
+        this is an The string to insert.
+    """
+
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "w") as w:
+        w.write(text)

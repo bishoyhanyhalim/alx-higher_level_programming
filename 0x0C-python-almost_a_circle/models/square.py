6a5dcdd668273698f21e628430150b1d9e2d65e1
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-02-16 19:23:28 -0800

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,22 @@ class Square(Rectangle):
 
     def __str__(self):
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    
+    def __update(self, id=None, size=None, x=None, y=None):
+        """privet func method"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """this public func args and kwargs"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)

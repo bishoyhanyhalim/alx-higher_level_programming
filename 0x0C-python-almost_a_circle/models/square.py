e3c575621e7721e22692fbdf9bfe866f5a32dd94
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-02-16 17:08:01 -0800

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this is a square class for project"""
+
+from models.rectangle import Rectangle
+# from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """this is a square class for project"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def size(self):
+        return self.width
+
+    def __str__(self):
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")

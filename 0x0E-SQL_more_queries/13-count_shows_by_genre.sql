36bbc4d468a3497111976b5f7d801ef04b6bcb0c
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-13 15:47:14 -0700

    13 again

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
-this iststask 13:wq
+-- this is task 13
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
